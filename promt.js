662f5dee1f3f7bcf697b6209da6b1fc73ccbb2c9
caravan4eg <alex.apanasik@gmail.com>
2020-03-06 10:56:52 +0300

    first commit

---
+++ promt.js
@@ -0,0 +1,20 @@
+// // prompt.js
+// let num = Number(prompt('Input number: '));
+// while (num > 0) {
+//    console.log('Hi, everybody: ' + num);
+//    num -= 1;
+// }
+
+// let yourName = 'Alex';
+// while (yourName != 'End') {
+//     yourName = prompt('Your name');
+//     console.log('Hi! ' + yourName);
+// }
+// console.log('Good buy!');
+
+
+
+
+for (let number = 0; number <= 10, number++) {
+    console.log(number);
+}
